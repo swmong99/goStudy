f3bda3e5a99d79ef35ad7688311c1726825f3e16
swmong99 <swmong99@naver.com>
2018-08-28 15:52:53 +0000

    타입 오류에 대한 예외처리 추가

--- gugudan.go
+++ gugudan.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
 
@@ -11,16 +8,19 @@ func main() {
 		fmt.Print("숫자를 입력하세요(종료는 0) : ")
 		var num int
 
-		fmt.Scanln(&num)
-		//fmt.Println(num)
-		//fmt.Println(reflect.TypeOf(num).String())
+		// Scanln의 리턴값을 이용하여 타입 체크를 한다
+		_, err := fmt.Scanln(&num)
 
-		if num == 0{
+		if err != nil {
+			fmt.Println("입력 오류입니다. 정수형 변수를 입력하세요.")
+
+			// 문자'열' 입력시 버퍼를 비우기 위함
+			var clearBuffer string
+			fmt.Scanln(&clearBuffer)
+		} else if num == 0 {
 			fmt.Println("프로그램을 종료합니다.")
 			break
 		} else if num < 2 || num > 9 {
-			fmt.Println("2 ~ 9사이의 값을 입력해주세요.")
-		} else if reflect.TypeOf(num).String() != "int" {
 			fmt.Println("2 ~ 9사이의 값을 입력해주세요.")
 		} else {
 			for i := 1; i<=9; i++ {
